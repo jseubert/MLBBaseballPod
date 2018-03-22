f98b5115fdef61ebde4eba03b539aa8710e5c6f8
John Seubert <john.seubert@airbnb.com>
2018-03-21 18:26:39 -0700

    Initial commit

---
+++ MLBBaseballPod/MLBBaseballPod.h
@@ -0,0 +1,19 @@
+//
+//  MLBBaseballPod.h
+//  MLBBaseballPod
+//
+//  Created by jseubert on 3/21/18.
+//  Copyright © 2018 jseubert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MLBBaseballPod.
+FOUNDATION_EXPORT double MLBBaseballPodVersionNumber;
+
+//! Project version string for MLBBaseballPod.
+FOUNDATION_EXPORT const unsigned char MLBBaseballPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLBBaseballPod/PublicHeader.h>
+
+
